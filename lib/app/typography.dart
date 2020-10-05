e3ab0a4d7c9a2f03421669670af8b1f6013d0b20
David Öhman <dohman_92@hotmail.com>
2020-10-05 23:32:04 +0200

    Remove some unnecessary type explicits

--- lib/app/typography.dart
+++ lib/app/typography.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 class AppTypography {
-  static const TextStyle number = TextStyle(
+  static const number = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.bold,
   );
