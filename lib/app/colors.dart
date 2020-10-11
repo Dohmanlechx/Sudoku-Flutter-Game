2c3ea41d5bfe841b740a58f280465d2a0555125f
David Öhman <dohman_92@hotmail.com>
2020-10-11 20:29:06 +0200

    Add splash screen

--- lib/app/colors.dart
+++ lib/app/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const white = Color(0xFFFFFFFF);
   static const red = Color(0xFFFF0000);
-  static const background = Color(0xFF99CCCC);
+  static const background = Color(0xFFD6E4EE);
   static const emptyTile = Color(0xFFEBEBEB);
 }
