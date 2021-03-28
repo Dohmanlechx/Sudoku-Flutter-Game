cd471a5b7938af8247ed220480740bedd7f49fb1
David Öhman <dohman_92@hotmail.com>
2021-03-28 10:35:45 +0200

    Nullsafety - Temporarily disable test_driver

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,11 +1,11 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:sudoku_game/main.dart' as app;
-
-/// To run, write this in the terminal:
-
-/// flutter drive --target=test_driver/app.dart
-
-void main() {
-  enableFlutterDriverExtension();
-  app.main();
-}
+// import 'package:flutter_driver/driver_extension.dart';
+// import 'package:sudoku_game/main.dart' as app;
+//
+// /// To run, write this in the terminal:
+//
+// /// flutter drive --target=test_driver/app.dart
+//
+// void main() {
+//   enableFlutterDriverExtension();
+//   app.main();
+// }
